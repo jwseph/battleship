dba941db9c66d3feeeb025f6028d5b79e183a505
Joseph Jackson <admin@kamiak.org>
2022-01-08 16:52:06 -0800

    cors fix

--- main.py
+++ main.py
@@ -12,9 +12,12 @@ from random import randint
 
 
 app = FastAPI()
+origins = [
+  'https://beta.kamiak.org/battleship'
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
